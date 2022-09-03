0beda4dcdd36c40b43efe99358f37ba47ed879d7
Hang Jiayu <88521229+Hang-Jiayu@users.noreply.github.com>
2022-09-03 19:02:23 +0800

    跟随启动器更新

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"github.com/fatih/color"
 	"github.com/imroc/req"
 	"os"
+	"time"
 )
 
 func main() {
 	client()
+	os.Exit(-1)
 }
 
 func client() {
@@ -19,6 +21,7 @@ func client() {
 			"If it still cannot be solved, please contact the administrator to solve the problem." +
 			"\n| Email: hempflower@outlook.com")
 	} else {
+		go Backstage_check_connect()
 		var command_line string
 		for {
 			fmt.Print("RSL> ")
@@ -26,30 +29,64 @@ func client() {
 			fmt.Scanln(&command_line)
 			//fmt.Print("\n")
 			if command_line == "/h" || command_line == "/help" {
-				show_command_list()
+				Help()
 			}
 			if command_line == "/exit" {
-				Simple("Press Any Key To Quit. . .")
-				fmt.Scanln(&command_line)
-				os.Exit(0)
+
 			}
 		}
 	}
 
 }
 
-func show_command_list() {
+func Help() {
 	Result("" +
 		"| Command List                                                         |\n" +
 		"| Commands                | Explain                                    |\n" +
 		"| /h or /help             | Get command list.                          |\n" +
+		"| /c                      | Clear the current input statement.         |\n" +
+		"| /exit                   | Exit the program.                          |\n" +
+		"| /add_dir                 | Add a new game dir.                        |\n" +
 		"")
 }
 
+func Exit(code int) {
+	Info("Press Any Key To Quit. . .")
+	input := ""
+	fmt.Scanln(&input)
+	os.Exit(code)
+}
+
+func Add_dir() {
+
+}
+
+func Check_connect() {
+	_, err := req.Post("http://127.0.0.1:30713/is_exist")
+	if err != nil {
+		Error(err.Error() + "\nUnable to connect to RedStone Launcher. Please check your Launcher to make sure it is started. " +
+			"If it still cannot be solved, please contact the administrator to solve the problem." +
+			"\n| Email: hempflower@outlook.com")
+		Exit(-2)
+	}
+}
+
 func GlobalInfo() {
-	Simple("Welcome to RedStone Launcher!")
-	Simple("Launcher version: 0.0.1_220901_Alpha.")
-	Simple("Type '/help' or '/h' for help. Type '/c' to clear the current input statement.\n")
+	fmt.Print(color.CyanString("Welcome to RedStone Launcher!\n" +
+		"Powered by TimeOut | TimeOoout@outlook.com\n" +
+		"Launcher version: 0.0.1_220901_Alpha.\n" +
+		"Type '/help' or '/h' for help. Type '/c' to clear the current input statement.\n"))
+}
+
+func Backstage_check_connect() {
+	_, err := req.Post("http://127.0.0.1:30713/is_exist")
+	if err != nil {
+		Error(err.Error() + "\nUnable to connect to RedStone Launcher. Please check your Launcher to make sure it is started. " +
+			"If it still cannot be solved, please contact the administrator to solve the problem." +
+			"\n| Email: hempflower@outlook.com")
+		Exit(-2)
+	}
+	time.Sleep(time.Second * 5)
 }
 
 func Result(format string, args ...interface{}) {
