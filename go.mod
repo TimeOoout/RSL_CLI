f5454e1eed70c1f47b84ee1a037202dbdb23d380
Hang Jiayu <88521229+Hang-Jiayu@users.noreply.github.com>
2022-09-03 23:50:30 +0800

    跟随启动器更新

--- go.mod
+++ go.mod
@@ -5,10 +5,14 @@ go 1.19
 require (
 	github.com/fatih/color v1.13.0
 	github.com/imroc/req v0.3.2
+	github.com/tidwall/sjson v1.2.5
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/tidwall/gjson v1.14.2 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"github.com/imroc/req"
+	"github.com/tidwall/sjson"
 	"os"
 	"time"
 )
+
+var host = "http://127.0.0.1:30713/"
+
+var debug = true
 
 func main() {
 	client()
@@ -22,17 +27,28 @@ func client() {
 			"\n| Email: hempflower@outlook.com")
 	} else {
 		go Backstage_check_connect()
+		if debug == true {
+			Debug("Debug:True")
+		}
 		var command_line string
+		fmt.Print("\n")
 		for {
-			fmt.Print("RSL> ")
+			fmt.Print("Launcher> ")
 			command_line = ""
 			fmt.Scanln(&command_line)
 			//fmt.Print("\n")
 			if command_line == "/h" || command_line == "/help" {
 				Help()
-			}
-			if command_line == "/exit" {
-
+			} else if command_line == "/exit" {
+				Exit(0)
+			} else if command_line == "/add_dir" {
+				Select("Path>")
+				path := ""
+				fmt.Scanln(&path)
+				Select("Name>")
+				name := ""
+				fmt.Scanln(&name)
+				Add_dir(path, name)
 			}
 		}
 	}
@@ -46,7 +62,7 @@ func Help() {
 		"| /h or /help             | Get command list.                          |\n" +
 		"| /c                      | Clear the current input statement.         |\n" +
 		"| /exit                   | Exit the program.                          |\n" +
-		"| /add_dir                 | Add a new game dir.                        |\n" +
+		"| /add_dir                | Add a new game directory.                  |\n" +
 		"")
 }
 
@@ -57,29 +73,34 @@ func Exit(code int) {
 	os.Exit(code)
 }
 
-func Add_dir() {
-
-}
-
-func Check_connect() {
-	_, err := req.Post("http://127.0.0.1:30713/is_exist")
+func Add_dir(path string, name string) {
+	head, _ := sjson.Set("", "Path", path)
+	head, _ = sjson.Set(head, "Name", name)
+	client_ := req.New()
+	content, err := client_.Post(host+"add_game_dir", head)
+	Debug(head)
 	if err != nil {
-		Error(err.Error() + "\nUnable to connect to RedStone Launcher. Please check your Launcher to make sure it is started. " +
-			"If it still cannot be solved, please contact the administrator to solve the problem." +
-			"\n| Email: hempflower@outlook.com")
-		Exit(-2)
+		Error("Failed to add a new game directory. Error:" + err.Error())
+	} else {
+		if content.String() == "" {
+			Info("Successfully added a new game directory!")
+		} else {
+			Error(content.String())
+		}
 	}
 }
 
 func GlobalInfo() {
-	fmt.Print(color.CyanString("Welcome to RedStone Launcher!\n" +
-		"Powered by TimeOut | TimeOoout@outlook.com\n" +
-		"Launcher version: 0.0.1_220901_Alpha.\n" +
-		"Type '/help' or '/h' for help. Type '/c' to clear the current input statement.\n"))
+	fun := color.New(color.Bold, color.FgCyan).PrintfFunc()
+	fun("RedStone Launcher [v0.0.1_220901_Alpha]\n" +
+		"Powered by TimeOut.\n" +
+		"Copyright (c) 2022 TimeOut. All rights reserved.\n" +
+		"Launcher version: v0.0.1_220901_Alpha.\n" +
+		"Type '/help' or '/h' for help. Type '/c' to clear the current input statement.\n")
 }
 
 func Backstage_check_connect() {
-	_, err := req.Post("http://127.0.0.1:30713/is_exist")
+	_, err := req.Post(host + "is_exist")
 	if err != nil {
 		Error(err.Error() + "\nUnable to connect to RedStone Launcher. Please check your Launcher to make sure it is started. " +
 			"If it still cannot be solved, please contact the administrator to solve the problem." +
@@ -89,26 +110,33 @@ func Backstage_check_connect() {
 	time.Sleep(time.Second * 5)
 }
 
+func Select(format string, args ...interface{}) {
+	fun := color.New(color.Bold, color.FgCyan).PrintfFunc()
+	fun(format, args...)
+}
+
 func Result(format string, args ...interface{}) {
-	fmt.Printf(color.CyanString("[Result] \n"+format+"\n", args...))
+	color.Cyan("[Result] \n"+format+"\n", args...)
 }
 
 func Info(format string, args ...interface{}) {
-	fmt.Printf(color.GreenString("[INFO] "+format+"\n", args...))
+	color.Green("[INFO] "+format+"\n", args...)
 }
 
 func Warn(format string, args ...interface{}) {
-	fmt.Printf(color.YellowString("[Warning] "+format+"\n", args...))
+	color.Yellow("[Warning] "+format+"\n", args...)
 }
 
 func Error(format string, args ...interface{}) {
-	fmt.Printf(color.RedString("[Error] "+format+"\n", args...))
+	color.Red("[Error] "+format+"\n", args...)
 }
 
 func Debug(format string, args ...interface{}) {
-	fmt.Printf(color.BlueString("[Debug] "+format+"\n", args...))
+	if debug == true {
+		color.Blue("[Debug] "+format+"\n", args...)
+	}
 }
 
 func Simple(format string, args ...interface{}) {
-	fmt.Printf(color.WhiteString(format+"\n", args...))
+	color.White(format+"\n", args...)
 }
